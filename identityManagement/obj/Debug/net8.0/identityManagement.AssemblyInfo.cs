362b0de843c845348df853fb2f394efe7e2e1884
Khaled Eid <khaled_eid77@outlook.com>
2025-02-06 17:41:10 +0200

    Dockerize The data base

--- identityManagement/obj/Debug/net8.0/identityManagement.AssemblyInfo.cs
+++ identityManagement/obj/Debug/net8.0/identityManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("identityManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88226b94c81812f5fa265b6e4899f451cc485c7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f763502291c6751f27399e674cf2b508a33d9641")]
 [assembly: System.Reflection.AssemblyProductAttribute("identityManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("identityManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
